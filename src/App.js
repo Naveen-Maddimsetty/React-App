0e4f3819bfd84ca9377eebd52586309167638996
Naveen-Maddimsetty <127311206+Naveen-Maddimsetty@users.noreply.github.com>
2023-06-06 10:41:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-return (
+return ;(
     <div class="foo">bar</div>
     <div className="App">
       <h1>Hello</h1>
