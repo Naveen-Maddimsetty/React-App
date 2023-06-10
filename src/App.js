ea591945cfa406a475f974d0e83d23678c3da1b9
Naveen-Maddimsetty <127311206+Naveen-Maddimsetty@users.noreply.github.com>
2023-06-10 15:37:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-import './App.css';
 import React from 'react';
 import logo from './logo.svg';
+import './App.css';
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                </header>
-            </div>
-            
-        );
-    }
-
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <h2>This is a React App</h2>
+    </div>
+  );
 }
 
-export default Home;
+export default App;
