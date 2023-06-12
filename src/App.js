56b99e551ee134f2a8a94fc384fdcd4d93d761a0
Naveen-Maddimsetty <127311206+Naveen-Maddimsetty@users.noreply.github.com>
2023-06-12 10:46:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class Home extends React.Component {
                 <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Edit <code>src/App.js</code> and save.
                 </p>
                 <a
                     className="App-link"
