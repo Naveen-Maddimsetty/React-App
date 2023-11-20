9fe3681c9981a7db047cc920fddf90bb1bd55d72
Naveen-Maddimsetty <127311206+Naveen-Maddimsetty@users.noreply.github.com>
2023-11-20 12:25:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <h2>This is the ReactApp</h2>
+      <h2>The React app is deployed in Jenkins</h2>
     </div>
   );
 }
